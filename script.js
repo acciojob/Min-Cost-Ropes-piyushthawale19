5affd96abf56ddbd39709139f20ef431c5d9bc8b
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-06-14 20:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+	 if (arr.length<=1) return 0;
+
+	let cost =0;
+
+	arr.sort((a,b)=> a-b);
+	while (arr.length>1){
+		let first = arr.shift();
+		let second = arr.shift();
+
+		let sum = first+ second;
+		cost+=sum;
+
+		let i=0;
+		while (i<arr.length && arr[i]<sum){
+			i++;
+		}  
+		arr.splice(i,0,arr)
+		
+	}
+	return cost;
   
 }
 
